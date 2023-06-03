81ccfa27c7660a7a80f9125ab1ccdaf2895ea3bb
Fredrik Kemling <kemling@gmail.com>
2023-06-03 20:21:16 +0200

    testar Spring Data REST

--- src/main/java/org/example/springboot/demo/mycoolapp/dao/EmployeeRepository.java
+++ src/main/java/org/example/springboot/demo/mycoolapp/dao/EmployeeRepository.java
@@ -3,9 +3,11 @@ package org.example.springboot.demo.mycoolapp.dao;
 
 import org.example.springboot.demo.mycoolapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     List<Employee> findByEmailIgnoreCase(String email);
